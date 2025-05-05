8ca381a90d0e76ff5501320739d5a40b2198070e
JoelFaldin <joelfaldin@gmail.com>
2025-05-05 13:06:23 -0400

    feat: adding abstract class example

---
+++ OOP/AbstractClass/src/abstractclass/Square.java
@@ -0,0 +1,21 @@
+package abstractclass;
+
+public class Square extends Figure {
+    
+    private double side;
+
+    public Square() {
+    }
+
+    public Square(double side, double x, double y) {
+        super(x, y);
+        this.side = side;
+    }
+    
+    @Override
+    public double calculateArea() {
+        double result = side * side;
+        return result;
+    }
+    
+}
