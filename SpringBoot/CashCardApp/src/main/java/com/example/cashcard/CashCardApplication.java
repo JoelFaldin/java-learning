f9081d485a7218c40f6610b83dc154c5f0fb046b
JoelF <joelfaldin@gmail.com>
2025-05-14 18:02:53 -0400

    refactor: changing app name and adding simple test

--- SpringBoot/CashCardApp/src/main/java/com/example/cashcard/CashCardApplication.java
+++ SpringBoot/CashCardApp/src/main/java/com/example/cashcard/CashCardApplication.java
@@ -1,13 +1,13 @@
-package com.springtest.api;
+package com.example.cashcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class CashCardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(CashCardApplication.class, args);
 	}
 
 }
--- SpringBoot/CashCardApp/src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ SpringBoot/CashCardApp/src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -1,10 +1,10 @@
-package com.springtest.api;
+package com.example.cashcard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class CashCardApplicationTests {
 
 	@Test
 	void contextLoads() {
