f8a57d18159669cda9a291f2384140f9838070b6
JoelF <joelfaldin@gmail.com>
2025-05-16 17:12:28 -0400

    feat: adding post route and its test

--- SpringBoot/cashcard/src/main/java/example/cashcard/CashCard.java
+++ SpringBoot/cashcard/src/main/java/example/cashcard/CashCard.java
@@ -2,7 +2,8 @@
 package example.cashcard;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
-public record CashCard (@Id Long id, Double amount) {
+public record CashCard (@Id @Column("ID") Long id, Double amount) {
 
 }
--- SpringBoot/cashcard/src/main/java/example/cashcard/CashCardRepository.java
+++ SpringBoot/cashcard/src/main/java/example/cashcard/CashCardRepository.java
@@ -1,7 +1,9 @@
 package example.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface CashCardRepository extends CrudRepository<CashCard, Long> {
 
 }
