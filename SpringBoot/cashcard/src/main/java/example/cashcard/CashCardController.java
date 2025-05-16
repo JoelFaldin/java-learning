9f58b3fd7a766e34972d6eec906fa2126da10404
JoelF <joelfaldin@gmail.com>
2025-05-15 20:39:58 -0400

    feat: injecting cashCardRepository and controller, and configuring schema.sql and data.sql to populate db

--- SpringBoot/cashcard/src/main/java/example/cashcard/CashCardController.java
+++ SpringBoot/cashcard/src/main/java/example/cashcard/CashCardController.java
@@ -5,17 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
 
+  private final CashCardRepository cashCardRepository;
+
+  private CashCardController(CashCardRepository cashCardRepository) {
+    this.cashCardRepository = cashCardRepository;
+  }
+
   @GetMapping("/{requestedId}")
   private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-    if (requestedId.equals(99L)) {
-      CashCard cashCard = new CashCard(99L, 123.45);
+    Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
 
-      return ResponseEntity.ok(cashCard);
+    // cashCardOptional.isPresent() can be true or false!
+    if (cashCardOptional.isPresent()) {
+      return ResponseEntity.ok(cashCardOptional.get());
     } else {
       return ResponseEntity.notFound().build();
     }
