100212c72a939f602d40e419fb9dbfddcd67635f
JoelF <joelfaldin@gmail.com>
2025-05-23 20:00:39 -0400

    feat: adding basic security config

---
+++ SpringBoot/cashcard/src/test/java/example/cashcard/SecurityConfig.java
@@ -0,0 +1,30 @@
+package example.cashcard;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .authorizeHttpRequests(request -> request
+        .requestMatchers("/cashcards/**")
+        .authenticated())
+      .httpBasic(Customizer.withDefaults())
+      .csrf(csrf -> csrf.disable());
+
+    return http.build();
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
