4d7988dbe017ff49eeecd682252ce55d4a7fe250
JoelF <joelfaldin@gmail.com>
2025-05-26 16:34:07 -0400

    feat: adding test to avoid updating someone elses cashcard

--- SpringBoot/cashcard/src/test/java/example/cashcard/CashCardApplicationTests.java
+++ SpringBoot/cashcard/src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -196,4 +196,16 @@ class CashCardApplicationTests {
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
   }
+
+  @Test
+  void shouldNotUpdateACashCardOwnedBySomeoneElse() {
+    CashCard bocchisCard = new CashCard(null, 333.33, null);
+    HttpEntity<CashCard> request = new HttpEntity<>(bocchisCard);
+
+    ResponseEntity<Void> response = restTemplate
+      .withBasicAuth("jowel", "abc123")
+      .exchange("/cashcards/102", HttpMethod.PUT, request, Void.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+  }
 }
